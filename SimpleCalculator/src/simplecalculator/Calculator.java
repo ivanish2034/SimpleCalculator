d3f2ba2e3a95ab54c1c9e3599e79530050ad7e77
ivanish2034 <ivanish.0119@gmail.com>
2025-02-17 17:32:07 +0300

    Commit2
    With CalculatorGUI

---
+++ SimpleCalculator/src/simplecalculator/Calculator.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package simplecalculator;
+
+/**
+ *
+ * @author ivis2
+ */
+public class Calculator {
+    
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+    
+}
